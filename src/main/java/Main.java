a64cb5b6cfbb3a3c1444c69055e2d6ac5636c7d8
Lance Tan <ltan0521@gmail.com>
2021-05-10 22:25:38 +0800

    Implemented creation of database tables

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,6 +6,7 @@ public class Main {
         PostgreSQLJDBC database = new PostgreSQLJDBC();
 
         database.getConnection();
+        database.createTables();
     }
 
 }
--- src/main/java/database/PostgreSQLJDBC.java
+++ src/main/java/database/PostgreSQLJDBC.java
@@ -4,11 +4,16 @@ import io.github.cdimascio.dotenv.Dotenv;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class PostgreSQLJDBC {
 
-    public Connection getConnection() {
-        Connection c = null;
+    Connection c;
+    Statement stmt;
+
+    public void getConnection() {
+        c = null;
         try {
             Class.forName("org.postgresql.Driver");
             c = DriverManager
@@ -20,7 +25,37 @@ public class PostgreSQLJDBC {
             System.exit(0);
         }
         System.out.println("Works");
-        return c;
+    }
+
+    public void createTables() {
+        try {
+            stmt = c.createStatement();
+
+            String remove1 = "DROP TABLE IF EXISTS guild_points";
+            stmt.executeUpdate(remove1);
+
+            String remove2 = "DROP TABLE IF EXISTS guild_predictions";
+            stmt.executeUpdate(remove2);
+
+            String createGuildPoints = "CREATE TABLE guild_points (" +
+                    "id INT PRIMARY KEY," +
+                    "guildName VARCHAR(20) NOT NULL," +
+                    "points INT NOT NULL)";
+            stmt.executeUpdate(createGuildPoints);
+
+            String createGuildPredictions = "CREATE TABLE guild_predictions (" +
+                    "predictionID INT PRIMARY KEY," +
+                    "gameNumber INT NOT NULL," +
+                    "teamName VARCHAR(5) NOT NULL," +
+                    "predictionDate VARCHAR(20) NOT NULL," +
+                    "betterID INT NOT NULL," +
+                    "FOREIGN KEY(betterID) REFERENCES guild_points(id))";
+            stmt.executeUpdate(createGuildPredictions);
+        } catch(SQLException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("here");
     }
 
 }
